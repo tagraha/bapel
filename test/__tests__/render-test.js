6259424b56ab9363ba9f464c14ff87fea8d78cb9
Tirta Nugraha <dev.nugrata@gmail.com>
2018-06-14 00:02:18 +0700

    test(renderer): remove uikit provider

--- test/__tests__/render-test.js
+++ test/__tests__/render-test.js
@@ -11,15 +11,13 @@ import UikitManifest from 'uikit-manifest';
 
 import ButtonStory from '../../stories/ButtonStory';
 
-import UikitProvider from '../../src/Provider';
+// import UikitProvider from '../../src/Provider';
 
 test('Render Button story correctly', () => {
   const mockUikitManifest = UikitManifest.getManifest();
   const tree = renderer
     .create(
-      <UikitProvider uikitManifest={mockUikitManifest}>
-        <ButtonStory />
-      </UikitProvider>
+      <ButtonStory />
     )
     .toJSON();
 
