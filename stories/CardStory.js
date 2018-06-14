28943983992c41e50bc0541dcd3e340aafca839b
Tirta Nugraha <dev.nugrata@gmail.com>
2018-06-15 00:05:23 +0700

    update usage snippet & adding preview label

--- stories/CardStory.js
+++ stories/CardStory.js
@@ -6,8 +6,15 @@ const CardStories = () => {
     <div className="doc-mainbar">
       <p>Usage: <code>{`import { Card } from bapel;`}</code></p>
       <br/>
-      <p><code>{`<Card />`}</code></p>
+      <p><pre>{`<Card
+  cardImage="https://ichef.bbci.co.uk/images/ic/720x405/p0517py6.jpg"
+  cardCaption="any kind of long text of title would be great. hehehe, yolo bro"
+  linkDestination="https://github.com/tagraha/bapel"
+  isReactRouter={false}
+  external={true}
+/>`}</pre></p>
       <br/>
+      <h3 style={{ color: 'red'}}>Preview</h3>
       <Card
         cardImage="https://ichef.bbci.co.uk/images/ic/720x405/p0517py6.jpg"
         cardCaption="any kind of long text of title would be great. hehehe, yolo bro"
