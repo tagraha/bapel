7f13b5bbb72102e0484ba73264ec7c384772248f
Tirta Nugraha <dev.nugrata@gmail.com>
2018-06-14 23:26:39 +0700

    refactor(styled): move styled component snippet at bottom

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Card = () => {
+  return (
+    <CardWrapper>
+      <CardImage src='https://ichef.bbci.co.uk/images/ic/720x405/p0517py6.jpg' />
+      <CardTitleWrapper>
+        <CardTitle href="javascript:void(0)">
+          any kind of long text of title would be great. hehehe, yolo bro
+        </CardTitle>
+      </CardTitleWrapper>
+
+      <CardContent>5m ago by Tirta</CardContent>
+    </CardWrapper>
+  )
+}
+
 const CardWrapper = styled.div`
   max-width: 33%;
 `;
@@ -29,20 +44,5 @@ const CardContent = styled.p`
   font-size: .75em;
   color: #b2b3bb;
 `
-
-const Card = () => {
-  return (
-    <CardWrapper>
-      <CardImage src='https://ichef.bbci.co.uk/images/ic/720x405/p0517py6.jpg' />
-      <CardTitleWrapper>
-        <CardTitle href="javascript:void(0)">
-          any kind of long text of title would be great. hehehe, yolo bro
-        </CardTitle>
-      </CardTitleWrapper>
-
-      <CardContent>5m ago by Tirta</CardContent>
-    </CardWrapper>
-  )
-}
 
 export default Card;
