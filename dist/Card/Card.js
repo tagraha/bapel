c5c0ae2afde670837ce76bcc024d228b8bceba83
Tirta Nugraha <dev.nugrata@gmail.com>
2018-06-15 16:02:39 +0700

    update build

--- dist/Card/Card.js
+++ dist/Card/Card.js
@@ -8,9 +8,9 @@ var _taggedTemplateLiteral2 = require('babel-runtime/helpers/taggedTemplateLiter
 
 var _taggedTemplateLiteral3 = _interopRequireDefault(_taggedTemplateLiteral2);
 
-var _templateObject = (0, _taggedTemplateLiteral3.default)(['\n  max-width: 33%;\n'], ['\n  max-width: 33%;\n']),
+var _templateObject = (0, _taggedTemplateLiteral3.default)(['\n  max-width: 33%;\n  outline: none !important;\n'], ['\n  max-width: 33%;\n  outline: none !important;\n']),
     _templateObject2 = (0, _taggedTemplateLiteral3.default)(['\n  width: 100%;\n  max-width: 500px;\n  max-height: 261px;\n'], ['\n  width: 100%;\n  max-width: 500px;\n  max-height: 261px;\n']),
-    _templateObject3 = (0, _taggedTemplateLiteral3.default)(['\n  padding: 1em 0 .5em 0;\n'], ['\n  padding: 1em 0 .5em 0;\n']),
+    _templateObject3 = (0, _taggedTemplateLiteral3.default)(['\n  padding: 1em 0 .5em 0;\n  text-decoration: none !important;\n'], ['\n  padding: 1em 0 .5em 0;\n  text-decoration: none !important;\n']),
     _templateObject4 = (0, _taggedTemplateLiteral3.default)(['\n  color: #000;\n  font-size: 1.2em;\n\n  &:hover {\n    color: #8e8e8e;\n    text-decoration: none;\n  }\n'], ['\n  color: #000;\n  font-size: 1.2em;\n\n  &:hover {\n    color: #8e8e8e;\n    text-decoration: none;\n  }\n']),
     _templateObject5 = (0, _taggedTemplateLiteral3.default)(['\n  font-size: .75em;\n  color: #b2b3bb;\n'], ['\n  font-size: .75em;\n  color: #b2b3bb;\n']);
 
@@ -22,7 +22,49 @@ var _styledComponents = require('styled-components');
 
 var _styledComponents2 = _interopRequireDefault(_styledComponents);
 
+var _reactRouterDom = require('react-router-dom');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Card = function Card(props) {
+  var cardImage = props.cardImage,
+      cardCaption = props.cardCaption,
+      linkDestination = props.linkDestination,
+      external = props.external,
+      isReactRouter = props.isReactRouter;
+
+
+  return _react2.default.createElement(
+    CardWrapper,
+    null,
+    _react2.default.createElement(CardImage, { src: cardImage || 'https://picsum.photos/500/300' }),
+    _react2.default.createElement(
+      CardTitleWrapper,
+      null,
+      cardCaption && !isReactRouter && _react2.default.createElement(
+        CardTitle,
+        {
+          href: linkDestination || "javascript:void(0)",
+          target: external ? '_blank' : '_parent',
+          rel: 'noopener noreferrer'
+        },
+        cardCaption
+      ),
+      isReactRouter && _react2.default.createElement(
+        LinkRouter,
+        {
+          to: linkDestination
+        },
+        cardCaption
+      )
+    ),
+    _react2.default.createElement(
+      CardContent,
+      null,
+      '5m ago by Tirta'
+    )
+  );
+};
 
 var CardWrapper = _styledComponents2.default.div(_templateObject);
 
@@ -34,26 +76,6 @@ var CardTitle = _styledComponents2.default.a(_templateObject4);
 
 var CardContent = _styledComponents2.default.p(_templateObject5);
 
-var Card = function Card() {
-  return _react2.default.createElement(
-    CardWrapper,
-    null,
-    _react2.default.createElement(CardImage, { src: 'https://ichef.bbci.co.uk/images/ic/720x405/p0517py6.jpg' }),
-    _react2.default.createElement(
-      CardTitleWrapper,
-      null,
-      _react2.default.createElement(
-        CardTitle,
-        { href: 'javascript:void(0)' },
-        'any kind of long text of title would be great. hehehe, yolo bro'
-      )
-    ),
-    _react2.default.createElement(
-      CardContent,
-      null,
-      '5m ago by Tirta'
-    )
-  );
-};
+var LinkRouter = (0, _styledComponents2.default)(_reactRouterDom.Link)(_templateObject4);
 
 exports.default = Card;
