585069ba88fbb2edbc0ea0ca8ea683917e716b2d
Tirta Nugraha <dev.nugrata@gmail.com>
2018-06-17 00:29:26 +0700

    adding github link

--- stories/IntroStory.js
+++ stories/IntroStory.js
@@ -5,6 +5,8 @@ const IntroStory = () => {
     <div className="doc-mainbar">
       <h1>Installation</h1>
       <p>npm: <code>{`npm install -S bapel`}</code></p>
+      <br/>
+      <p>github: <a href="https://github.com/tagraha/bapel" target="_blank" rel="noopener noreferrer">https://github.com/tagraha/bapel</a></p>
     </div>
   );
 }
