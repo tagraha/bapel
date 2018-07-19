7a8e4221333734295ebd10cf99d023ca933ab614
Tirta Nugraha <dev.nugrata@gmail.com>
2018-07-19 14:16:42 +0700

    fix(component): fix Grid Component

--- src/Grid/components/Grid.js
+++ src/Grid/components/Grid.js
@@ -1,50 +1,20 @@
-import getClass from '../classNames';
 import React from 'react';
 import PropTypes from 'prop-types';
 import createProps from '../createProps';
-import { ViewportSizeType } from '../types';
-
-const rowKeys = ['start', 'center', 'end', 'top', 'middle', 'bottom', 'around', 'between'];
+import getClass from '../classNames';
 
 const propTypes = {
-  reverse: PropTypes.bool,
-  start: ViewportSizeType,
-  center: ViewportSizeType,
-  end: ViewportSizeType,
-  top: ViewportSizeType,
-  middle: ViewportSizeType,
-  bottom: ViewportSizeType,
-  around: ViewportSizeType,
-  between: ViewportSizeType,
+  fluid: PropTypes.bool,
   className: PropTypes.string,
   tagName: PropTypes.string,
   children: PropTypes.node
 };
 
-function getRowClassNames(props) {
-  const modificators = [props.className, getClass('row')];
+export default function Grid(props) {
+  const containerClass = getClass(props.fluid ? 'container-fluid' : 'container');
+  const classNames = [props.className, containerClass];
 
-  for (let i = 0; i < rowKeys.length; ++i) {
-    const key = rowKeys[i];
-    const value = props[key];
-    if (value) {
-      modificators.push(getClass(`${key}-${value}`));
-    }
-  }
-
-  if (props.reverse) {
-    modificators.push(getClass('reverse'));
-  }
-
-  return modificators;
+  return React.createElement(props.tagName || 'div', createProps(propTypes, props, classNames));
 }
 
-export function getRowProps(props) {
-  return createProps(propTypes, props, getRowClassNames(props));
-}
-
-export default function Row(props) {
-  return React.createElement(props.tagName || 'div', getRowProps(props));
-}
-
-Row.propTypes = propTypes;
+Grid.propTypes = propTypes;
