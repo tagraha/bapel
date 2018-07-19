74649ea62158b41f3de7d1428a8ea879427cc6c3
Tirta Nugraha <dev.nugrata@gmail.com>
2018-07-19 13:58:56 +0700

    build(loader): adding css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,11 @@ module.exports = {
           },
         ],
       },
+
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
     ],
   },
   plugins: [new WebpackAssetsManifest()],
