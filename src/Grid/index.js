7155580dcef79df3fb79a7f1e57ac2984851c839
Tirta Nugraha <dev.nugrata@gmail.com>
2018-07-19 13:46:09 +0700

    fix(component): fix export path not resolved

--- src/Grid/index.js
+++ src/Grid/index.js
@@ -1,10 +1,10 @@
-import Grid from 'components/Grid';
-import Row from 'components/Grid';
-import Col from 'components/Col';
+import Grid from './components/Grid';
+import { Row, getRowProps } from './components/Row';
+import { Col, getColumnProps } from './components/Col';
 
-export { Grid } from 'components/Grid';
-export { Row, getRowProps } from 'components/Row';
-export { Col, getColumnProps } from 'components/Col';
+// export { Grid } from 'components/Grid';
+// export { Row, getRowProps } from 'components/Row';
+// export { Col, getColumnProps } from 'components/Col';
 
 Grid.Row = Row;
 Grid.Col = Col;
