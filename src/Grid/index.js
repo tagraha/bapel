965ed5a7650ccfbd361609993abb03e5839fcc50
Tirta Nugraha <dev.nugrata@gmail.com>
2018-07-19 13:37:58 +0700

    fix(component): fix exported Grid component

--- src/Grid/index.js
+++ src/Grid/index.js
@@ -1,3 +1,12 @@
+import Grid from 'components/Grid';
+import Row from 'components/Grid';
+import Col from 'components/Col';
+
 export { Grid } from 'components/Grid';
 export { Row, getRowProps } from 'components/Row';
 export { Col, getColumnProps } from 'components/Col';
+
+Grid.Row = Row;
+Grid.Col = Col;
+
+export default Grid;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@
 // @create-index
 export {default as Button} from './Button';
 export {default as Card} from './Card';
+export {default as Grid} from './Grid';
 export {BapelUiKit as default, UikitConsumer, UikitProvider} from './Provider';
