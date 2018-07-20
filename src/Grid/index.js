6359b7422a609118e19c2feb3708a2a30da645fa
Tirta Nugraha <dev.nugrata@gmail.com>
2018-07-21 01:22:24 +0700

    fix(component): fix grid

--- src/Grid/index.js
+++ src/Grid/index.js
@@ -2,9 +2,9 @@ import Grid from './components/Grid';
 import { Row, getRowProps } from './components/Row';
 import { Col, getColumnProps } from './components/Col';
 
-// export { Grid } from 'components/Grid';
-// export { Row, getRowProps } from 'components/Row';
-// export { Col, getColumnProps } from 'components/Col';
+export { Grid } from './components/Grid';
+export { Row, getRowProps } from './components/Row';
+export { Col, getColumnProps } from './components/Col';
 
 Grid.Row = Row;
 Grid.Col = Col;
