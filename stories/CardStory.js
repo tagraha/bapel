e2ef3bedfa1c16edb77394f5b3418e785c00e177
Tirta Nugraha <dev.nugrata@gmail.com>
2018-07-24 16:08:54 +0700

    fix(validateDOMNesting): fixing pre inside of p html tag

--- stories/CardStory.js
+++ stories/CardStory.js
@@ -6,13 +6,13 @@ const CardStories = () => {
     <div className="doc-mainbar">
       <p>Usage: <code>{`import { Card } from bapel;`}</code></p>
       <br/>
-      <p><pre>{`<Card
+      <div><pre>{`<Card
   cardImage="https://ichef.bbci.co.uk/images/ic/720x405/p0517py6.jpg"
   cardCaption="any kind of long text of title would be great. hehehe, yolo bro"
   url="https://github.com/tagraha/bapel"
   isReactRouter={false}
   external={true}
-/>`}</pre></p>
+/>`}</pre></div>
       <br/>
       <h3 style={{ color: 'red'}}>Preview</h3>
       <Card
