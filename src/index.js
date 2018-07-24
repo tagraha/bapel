bd2159b0b42f3eb9a283bfc0c2020fc302f47e49
Tirta Nugraha <dev.nugrata@gmail.com>
2018-07-24 16:16:24 +0700

    remove grid component

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@
 // @create-index
 export {default as Button} from './Button';
 export {default as Card} from './Card';
-export {default as Grid} from './Grid';
 export {BapelUiKit as default, UikitConsumer, UikitProvider} from './Provider';
