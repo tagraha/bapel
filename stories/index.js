f984097168d9e328b39795c152b0b7f03e663f0a
Tirta Nugraha <dev.nugrata@gmail.com>
2018-07-24 16:07:05 +0700

    other(component): remove Grid Component

--- stories/index.js
+++ stories/index.js
@@ -6,7 +6,6 @@ import {storiesOf} from '@storybook/react';
 import IntroStory from './IntroStory';
 import ButtonStory from './ButtonStory';
 import CardStory from './CardStory';
-import GridStory from './GridStory';
 // import ViewStory from './ViewStory';
 // import AvatarStory from './AvatarStory';
 // import TextStory from './TextStory';
@@ -24,13 +23,5 @@ import GridStory from './GridStory';
 storiesOf('Bapel Components', module)
   .addDecorator(story => <Provider>{React.createElement(story)}</Provider>)
   .add('Introduction', () => <IntroStory />)
-  .add('Grid', () => <GridStory />)
   .add('Card', () => <CardStory />)
   .add('Button', () => <ButtonStory />);
-
-// storiesOf('bapel BlockUI', module)
-//   .add('NavSlide', () => <NavSlideStory />)
-//   .add('Follow', () => <FollowStory />)
-//   .add('Topic', () => <TopicStory />)
-//   .add('Profile', () => <ProfileStory />)
-//   .add('Tab', () => <TabStory />);
