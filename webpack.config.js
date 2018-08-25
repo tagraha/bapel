0044de5fe68d1cd8a6366a3a569cee3eac265f83
Tirta Nugraha <dev.nugrata@gmail.com>
2018-08-26 01:52:08 +0700

    style mock

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const entry = require('webpack-glob-entry');
 const WebpackAssetsManifest = require('webpack-assets-manifest');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: entry('./assets/icons/*.*', './assets/logos/*.*'),
+  entry: entry('./assets/**/*.*'),
   output: {
     path: path.resolve('./dist/public/'),
     filename: '[name]-[hash].js',
@@ -11,7 +13,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(png|jpg|gif|svg|woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(png|jpg|gif|svg)$/,
         use: [
           {
             loader: 'file-loader',
@@ -21,7 +23,41 @@ module.exports = {
           },
         ],
       },
+
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+        ]
+      },
     ],
   },
-  plugins: [new WebpackAssetsManifest()],
+  plugins: [
+    new WebpackAssetsManifest(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin()
+    ]
+  }
 };
